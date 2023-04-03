c3737f10cbf3651aff14acc286b09debb2f08fbf
Mykola Saliuk <haker0502@ukr.net>
2023-04-03 16:05:23 +0300

    чистить код

--- middlewares/isValidId.js
+++ middlewares/isValidId.js
@@ -5,7 +5,6 @@ const { RequestError } = require("../helpers");
 const isValidId = (req, _, next) => {
   const { contactId } = req.params;
   const isCorrectId = isValidObjectId(contactId);
-  console.log(contactId);
   if (!isCorrectId) {
     const error = RequestError(400, `${contactId} is not correct id format`);
     // const error = new Error(`'${contactId}' is not correct id format`);
