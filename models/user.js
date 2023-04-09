1cd917cc6316cc5ed2611bfb6b25eb3f2cf88ec6
Mykola Saliuk <haker0502@ukr.net>
2023-04-09 22:15:19 +0300

    створює схему і модель користувача для колекції users

---
+++ models/user.js
@@ -0,0 +1,29 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema({
+  password: {
+    type: String,
+    required: [true, "Set password for user"],
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+  },
+  subscription: {
+    type: String,
+    enum: ["starter", "pro", "business"],
+    default: "starter",
+  },
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: "user",
+  },
+  token: String,
+});
+
+const User = model("user", userSchema);
+
+module.exports = {
+  User,
+};
