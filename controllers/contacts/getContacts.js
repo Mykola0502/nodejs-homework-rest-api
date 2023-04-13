6fccea63fd3ea55daf2f6bf3b0831005f188ee11
Mykola Saliuk <haker0502@ukr.net>
2023-04-14 00:06:51 +0300

    додає пагінацію

--- controllers/contacts/getContacts.js
+++ controllers/contacts/getContacts.js
@@ -2,7 +2,16 @@ const { Contact } = require("../../models");
 
 const getContacts = async (req, res) => {
   const { _id: owner } = req.user;
-  const result = await Contact.find({ owner }).populate(
+
+  let { page = 1, limit = 5 } = req.query;
+  const parsedPage = parseInt(page);
+  const parsedLimit = parseInt(limit);
+  page = parsedPage >= 1 ? parsedPage : 1;
+  limit = parsedLimit > 20 || parsedLimit < 1 ? 20 : parsedLimit;
+
+  const skip = (parseInt(page) - 1) * limit;
+
+  const result = await Contact.find({ owner }, "", { skip, limit }).populate(
     "owner",
     "email subscription"
   );
