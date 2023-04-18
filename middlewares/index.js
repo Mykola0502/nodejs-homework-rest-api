772ac01cbbbca183c488e747b9843f548708b3f1
Mykola Saliuk <haker0502@ukr.net>
2023-04-18 23:38:49 +0300

    робить налаштування проекту, у схему користувача додає нову властивість avatarURL, додає шаблонну аватарку при реєстрації користувача

--- middlewares/index.js
+++ middlewares/index.js
@@ -2,10 +2,12 @@ const { validationBody } = require("./validation");
 const ctrlWrapper = require("./ctrlWrapper");
 const isValidId = require("./isValidId");
 const authenticate = require("./authenticate");
+const upload = require("./upload");
 
 module.exports = {
   validationBody,
   ctrlWrapper,
   isValidId,
   authenticate,
+  upload,
 };
--- models/user.js
+++ models/user.js
@@ -16,6 +16,7 @@ const userSchema = new Schema(
       required: [true, "Email is required"],
       unique: true,
     },
+    avatarURL: String,
     subscription: {
       type: String,
       enum: subscriptionList,
