7c71796e1abebc055360ef8e3fbfe78b7b5695ca
Mykola Saliuk <haker0502@ukr.net>
2023-04-20 02:14:58 +0300

    Обробляє аватарку пакетом jimp і ставить для неї розміри 250 на 250

--- controllers/users/updateAvatar.js
+++ controllers/users/updateAvatar.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const fs = require("fs/promises");
+const Jimp = require("jimp");
 const { User } = require("../../models");
 
 const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
@@ -7,17 +8,30 @@ const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
 const updateAvatar = async (req, res) => {
   const { _id } = req.user;
   const { path: tmpUpload, originalname } = req.file;
-  const filename = `${_id}_${originalname}`;
+  const avatarName = `${_id}_${originalname}`;
   //   const [, extension] = originalname.split(".");
-  //   const filename = `${_id}.${extension}`;
-  const resultUpload = path.join(avatarsDir, filename);
-  await fs.rename(tmpUpload, resultUpload);
-  const avatarURL = path.join("avatars", filename);
-  await User.findByIdAndUpdate(_id, { avatarURL });
+  //   const avatarName = `${_id}.${extension}`;
 
-  res.json({
-    avatarURL,
-  });
+  try {
+    const img = await Jimp.read(tmpUpload);
+    await img.resize(250, 250);
+    await img.write(tmpUpload);
+
+    const resultUpload = path.join(avatarsDir, avatarName);
+
+    await fs.rename(tmpUpload, resultUpload);
+
+    const avatarURL = path.join("avatars", avatarName);
+
+    await User.findByIdAndUpdate(_id, { avatarURL });
+
+    res.json({
+      avatarURL,
+    });
+  } catch (error) {
+    await fs.unlink(tmpUpload);
+    throw error;
+  }
 };
 
 module.exports = updateAvatar;
