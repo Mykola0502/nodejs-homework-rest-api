198d458f458f74e24c049ee9b8ec798df06f7767
Mykola Saliuk <haker0502@ukr.net>
2023-04-20 00:31:35 +0300

    створює ендпоінт /users/avatars

---
+++ controllers/users/updateAvatar.js
@@ -0,0 +1,23 @@
+const path = require("path");
+const fs = require("fs/promises");
+const { User } = require("../../models");
+
+const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
+
+const updateAvatar = async (req, res) => {
+  const { _id } = req.user;
+  const { path: tmpUpload, originalname } = req.file;
+  const filename = `${_id}_${originalname}`;
+  //   const [, extension] = originalname.split(".");
+  //   const filename = `${_id}.${extension}`;
+  const resultUpload = path.join(avatarsDir, filename);
+  await fs.rename(tmpUpload, resultUpload);
+  const avatarURL = path.join("avatars", filename);
+  await User.findByIdAndUpdate(_id, { avatarURL });
+
+  res.json({
+    avatarURL,
+  });
+};
+
+module.exports = updateAvatar;
